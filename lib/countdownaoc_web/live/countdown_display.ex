0cc0e0c80f5b84d657f8330c84733d3e349d6f5d
Teo Diaz <teo.diaz@cabify.com>
2021-12-07 10:51:56 +0100

    day8

--- lib/countdownaoc_web/live/countdown_display.ex
+++ lib/countdownaoc_web/live/countdown_display.ex
@@ -32,7 +32,7 @@ defmodule CountdownaocWeb.CountdownDisplay do
     d1 = %DateTime{
       year: 2021,
       month: 12,
-      day: 7,
+      day: 8,
       zone_abbr: "RST",
       hour: 5,
       minute: 0,
